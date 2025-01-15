60b0fb562e02027974824af2ffca2ef87afc889f
PC <gass.lyna@gmail.com>
2025-01-15 20:24:49 +0100

    commt main program

--- src/main/java/tn/pi/ManageRecruitment/ManageRecruitmentApplication.java
+++ src/main/java/tn/pi/ManageRecruitment/ManageRecruitmentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ManageRecruitmentApplication {
 
+	//prg princaple
 	public static void main(String[] args) {
 		SpringApplication.run(ManageRecruitmentApplication.class, args);
 	}
